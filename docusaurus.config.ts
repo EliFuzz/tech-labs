263708c44c27d6b83ebc48d01d1a336407adeef6
Roman Glushach <roman.glushach@walmart.com>
2024-12-01 13:42:11 -0500

    Code highlight theme

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -91,8 +91,8 @@ const config: Config = {
       ],
     },
     prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
+      theme: prismThemes.oneLight,
+      darkTheme: prismThemes.oneDark,
       additionalLanguages: ["java"],
     },
     mermaid: {
